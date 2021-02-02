e94ab62c0da31564eb9195a3f6fac354170a196c
Will Andre <wandre94@gmail.com>
2021-02-02 21:04:00 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,9 +13,7 @@ class Artist
   end
 
   def songs
-    Song.all.select { |song| if song.artist == self
-    song.artist
-  end }
+    Song.all.select { |song| song.artist == self }
   end
 
   def new_song(name, genre)
