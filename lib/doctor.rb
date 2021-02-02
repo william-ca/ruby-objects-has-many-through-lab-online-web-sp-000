34a2ce3aad5d64a8b231c8d8cca07515ebe0634a
Will Andre <wandre94@gmail.com>
2021-02-02 21:38:47 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -11,5 +11,17 @@ class Doctor
   def self.all
     @@all
   end
+
+  def appointments
+    Appointment.all.select { |apt| apt.doctor == self }
+  end
+
+  def new_appointment(date, patient)
+    Appointment.new(date, patient, self)
+  end
+
+  def patients
+    Appointment.map { |apt| apt.patients }
+  end
   
 end
