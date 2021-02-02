2dd9f7d12f9b9b42ca834149c2390688593b6fd1
Will Andre <wandre94@gmail.com>
2021-02-02 21:38:41 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -5,6 +5,23 @@ class Patient
 
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def new_appointment(date, doctor)
+    Appointment.new(date, self, doctor)
+  end
+
+  def self.all
+    @@all
+  end
+
+  def appointments
+    Appointment.all.select { |apt| apt.patient == self }
+  end
+
+  def doctors
+    Appointment.map { |apt| apt.doctor }
   end
   
 end
